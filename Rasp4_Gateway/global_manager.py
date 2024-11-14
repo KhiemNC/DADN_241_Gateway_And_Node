e602587216ce79cc8293bf4bf347e033a69bd42b
KhiemNC <nguyencongkhiem510@gmail.com>
2024-11-14 14:17:21 +0700

    Split topic, only subcribe to which is necessary. Make mySerialManager a global variable.

--- Rasp4_Gateway/global_manager.py
+++ Rasp4_Gateway/global_manager.py
@@ -1,10 +1,19 @@
 import aws_mqtt
+import data_processing
+import serial_manager
 
 value_topics = ["values"]
-communicate_topics = ["communicate/clienttoserver", "communicate/servertoclient"]
+receiving_topic = ["communicate/clienttoserver"]
+transfering_topic = ["communicate/servertoclient"]
 error_topics = ["dev/error"]
 others_topics = ["others"]
-all_topics = value_topics + communicate_topics + error_topics + others_topics
+all_topics = value_topics + receiving_topic + transfering_topic + error_topics + others_topics
+need_to_subscribe = receiving_topic + error_topics
+
+def on_message_received(topic, payload, dup, qos, retain, **kwargs):
+    print("RECEIVED MESSAGE FROM TOPIC '{}': {}".format(topic, payload))
+    if (topic == receiving_topic[0]):
+        data_processing.process_data_from_client_to_device(payload)
 
 myAwsMqtt = aws_mqtt.AWS_MQTT(
     endpoint="a3pm7mxg4x4xbb-ats.iot.ap-southeast-2.amazonaws.com",
@@ -12,6 +21,9 @@ myAwsMqtt = aws_mqtt.AWS_MQTT(
     cert_file="./AWS_Connection_Kit/DADN_Rasp4_Gateway.cert.pem",
     private_key="./AWS_Connection_Kit/DADN_Rasp4_Gateway.private.key",
     client_id="DADN-Python-Gateway",
-    topic=all_topics,
-    count=0
+    topic=need_to_subscribe,
+    count=0,
+    on_message_received=on_message_received
 )
+
+mySerialManager = None
--- Rasp4_Gateway/main.py
+++ Rasp4_Gateway/main.py
@@ -1,21 +1,17 @@
-import aws_mqtt
-import serial_manager
-import serial
 import time
-import json
-import random
-from global_manager import myAwsMqtt
+import global_manager
+import serial_manager
 
 if __name__ == '__main__':
-    myAwsMqtt.connect()
-    myAwsMqtt.subscribe()
+    global_manager.myAwsMqtt.connect()
+    global_manager.myAwsMqtt.subscribe()
     
     counter = 0
     
     # Open serial port 
-    mySerialManager = serial_manager.SerialManager()
-    mySerialManager.open_serial_port(baudrate=115200)
+    global_manager.mySerialManager = serial_manager.SerialManager()
+    global_manager.mySerialManager.open_serial_port(baudrate=115200)
 
     while True:
-        mySerialManager.read_data_serial()
+        global_manager.mySerialManager.read_data_serial()
         time.sleep(1)
